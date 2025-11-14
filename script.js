cd51572231a023ddac931794eda4d50ab33333be
giovanimaori-alt <giovani.maori@gmail.com>
2025-11-14 12:07:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ window.addEventListener("DOMContentLoaded", () => {
   /* =====================
      BADGE VERSION
      ===================== */
-  const version = "bêta V.0.9";
+  const version = "bêta V.0.10";
 
   if (!document.getElementById("versionBadge")) {
     const badge = document.createElement("div");
